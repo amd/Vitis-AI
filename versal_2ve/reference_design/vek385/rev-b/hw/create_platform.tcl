8296b963ee6837982b8f9679a1ef9b100eed9919
Veerender Sura <vsura@amd.com>
2026-06-08 17:21:19 +0530

    Remove redundant files and add comments

--- versal_2ve/reference_design/vek385/rev-b/hw/create_platform.tcl
+++ versal_2ve/reference_design/vek385/rev-b/hw/create_platform.tcl
@@ -14,12 +14,11 @@ set_property ip_repo_paths ./custom_ips [current_project]
 update_ip_catalog
 
 # Import CED design and update it to add required custom changes
-#xhub::refresh_catalog [xhub::get_xstores Vivado_example_project]
 create_bd_design "bd" -mode batch
 instantiate_example_design -template xilinx.com:design:versal_comn_platform:2.0 -design bd -options {Design_type.VALUE Extensible Include_AIE.VALUE true }
-#instantiate_example_design -template xilinx.com:design:versal_gen2_platform:1.1 -design bd -options { Board_selection.VALUE VEK385 Design_type.VALUE Extensible}
 update_compile_order -fileset sources_1
 
+# Apply Vitis AI specific block design customizations (NoC, LPDDR5X, DDRMC5) on top of CED design
 source pfm_bd.tcl
 
 # Source platform ports
@@ -37,6 +36,8 @@ make_wrapper -files [get_files $project_name.srcs/sources_1/bd/bd/bd.bd] -top
 add_files -norecurse $project_name.gen/sources_1/bd/bd/hdl/bd_wrapper.v
 add_files -norecurse $project_name.srcs/sources_1/bd/bd/bd.bd
 
+# Ignore the CED’s imported golden NCR
+# Let Vivado generate a fresh NoC solution during impl_1 for the modified platform
 set_property is_enabled false [get_files  ./example_design.srcs/utils_1/imports/golden_ncr/vek385_revb_6280184_0xc546f4d8.ncr]
 set_property NOC_SOLUTION_FILE "" [get_runs impl_1]
 
@@ -69,19 +70,19 @@ if {$pre_synth} {
   launch_runs synth_1 -jobs 20
   wait_on_run synth_1
  
-# adding it as workaround for CR-1238158
+  # adding it as workaround for pl overlay pdi programming failure
   set_param noc.enableNOCClockGating false
 
-# Implementation Run
+  # Implementation Run
   launch_runs impl_1 -to_step write_device_image
   wait_on_run impl_1
 
   open_run impl_1
 
+  # Protects the static/platform portion of the NoC solution when PL is reconfigured later for segmented boot
   set_property lock true [get_noc_net_routes -of [get_noc_logical_path -filter initial_boot]]
-  write_noc_solution -force -file impl_out_noc_solution_2.ncr
   
-# Generating dynamic reload extensible XSA as default hardware platform
+  # Generating dynamic reload extensible XSA as default hardware platform
   write_hw_platform -fixed -include_bit -force -file ./example_design_pfm_fixed.xsa
   write_hw_platform -hw -force -file ./example_design_pfm_extensible.xsa
   puts "Generation of both post_implementation fixed and extensible xsa completed"
